a814c8976f3c9d707cd5662084006d86a688fd24
Felix Lidforsen <felixlidforsen@gmail.com>
2019-02-07 13:30:41 +0100

    sendEmail

--- models/sendEmail.js
+++ models/sendEmail.js
@@ -1,33 +1,16 @@
-// errormailer works only in production mode
-process.env.NODE_ENV = 'production';
-
 var nodemailer = require('nodemailer');
-var errormailer = require("../");
-var connect = require("connect");
-
-// Create a SMTP transport object
-var transport = nodemailer.createTransport("Test", {
-  service: 'Gmail', // use well known service
-  auth: {
-    user: process.env.agnes.ekfors.elvin,//Ändra
-    pass: process.env.Mattfrans10
-  }
+var transporter = nodemailer.createTransport({
+    service: 'Gmail',
+    auth: { 
+        user: 'pulsenfelrapport@gmail.com',
+        pass: 'slagthuset'
+    }
 });
 
-console.log('SMTP Configured');
-
-var errorHandler = errormailer(transport, {
-  subject: "Testing errormailer!",
-  to: "agnes.ekfors.elvin@gmail.com"
+console.log('created');
+transporter.sendMail({
+from: 'pulsenfelrapport@gmail.com',
+  to: 'agnes.ekfors@hotmail.com',
+  subject: 'SECURITY ALERT',
+  text: 'Din app e knas bror'
 });
-
-var funcErr = function(req, res) { throw new Error("AHAHH"); };
-
-var app = connect().
-  use(funcErr).
-  use(errorHandler).
-  use(connect.errorHandler({dumpException: true, showStack: true})).
-  listen(process.env.PORT || 3000, 
-         function() {
-           console.log("ErrorMailer connect demo started at port 3000");
-         });
