5e5f7c1c67048b8182f8a8107581028150aedfac
Erma32 <erikgullberg97@gmail.com>
2019-02-13 18:18:53 +0100

    Added almost complete functionality to the program.

--- index.js
+++ index.js
@@ -3,7 +3,10 @@ var fs = require('fs');
 var elasticsearch = require('elasticsearch');
 var nodemailer = require('nodemailer');
 var dateAndTime = new Date();
-var repsonseObject;
+var responseObject;
+var errorLogIDs = [];
+var newLogs;
+var elasticQueryData = [];
 
 //Method and Variable for reading login information
 var readFile = fs.readFileSync('./models/assets/loginCredentials.json', 'utf8');
@@ -34,11 +37,48 @@ client.search({
     index: 'errorlogs',
     type: 'posts',
     q: 'PostType:Log'
-}).then(function(resp){ 
+}).then(function(resp){
+    //Code below triggers if something was found 
     if (resp.hits.max_score != null && resp.hits.total != 0){
-        repsonseObject = resp;
-        console.log("I found an error log");
-        mailService();
+        console.log("AlertsAlot found an error log");
+        responseObject = resp;
+        
+        //Declare an empty array and add all id:s found in the query
+        var elasticQueryIDs = [];
+        resp.hits.hits.forEach(element => {
+            elasticQueryIDs.push(element._id);
+        });
+
+        resp.hits.hits.forEach(element => {
+           elasticQueryData.push("--Start of file-- \n"); 
+           elasticQueryData.push(JSON.stringify(element._source));
+           elasticQueryData.push("\n--End of file--");
+        });
+
+
+        //Run compareArrays()
+        //If the contents of the arrays do not match: add and save the found id:s. Then trigger sendMail()
+        //Declare newLogs as the new number of logs
+        if (compareArrays(elasticQueryIDs, errorLogIDs) == false){
+            newLogs = parseInt(responseObject.hits.total) - errorLogIDs.length
+            resp.hits.hits.forEach(element => {
+                errorLogIDs.push(element._id);
+            })
+            console.log("A new error log ID has been found. Sending a mail");
+            mailService();
+        }else{
+            console.log("Found error logs with a previously alerted ID. A mail has not been sent");
+        };
+        
+        //compareArrays()
+        //Take the values of both arrays and sort them.
+        //Compare the sorted arrays and return a boolean
+        function compareArrays(elasticQueryIDs, errorLogIDs){
+        elasticQueryIDs.map( function (x){ return x._id; } ).sort();
+        errorLogIDs.map( function (x){ return x._id; } ).sort();
+        return (elasticQueryIDs.join(',') == errorLogIDs.join(','));
+        };
+
     }else{
         console.log("No Error logs found");
     }
@@ -61,17 +101,19 @@ function mailService(){
         user:"pulsen@erikgullberg.se",
         pass:"felix123"
       }
-    });
+    }); 
     
     //Define Message options
     //Parameters can be pulled from properties file using jsonContent2.xxx
     var message = {
       from: "pulsen@erikgullberg.se",
       to: jsonContent2.email,
-      subject: "AlertsAlot: Error log found!",
+      subject: "AlertsAlot: Error log(s) found!!",
       text: "Hello " + jsonContent2.name + ". \n \n"
-      + "AlertsAlot has detected " + repsonseObject.hits.total + " error log(s). \n"
-      + "The log was found on " + dateAndTime + ". \n"
+      + "AlertsAlot has detected " + newLogs + " error log(s). \n"
+      + "The log(s) were found on " + dateAndTime + ". \n"
+      + "The log(s) contain(s) the following data: \n"
+      + elasticQueryData + "\n"
       + "Please check your elasticsearch database."
     };
     
